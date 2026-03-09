8b8346ab7a4d3263c1016d863b75d1c74d096227
Alashmony2 <ahmedSamyAlashmony1@gmail.com>
2026-03-09 23:25:30 +0200

    add hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
+import Image from "next/image.js";
+
 export default function Home() {
   return <>
-    <h1 className="text-3xl font-bold underline m-3">Eng Alashmony</h1>
+    <section className="relative w-full h-60">
+      <Image fill className="object-cover" src="/back.png" alt="background" />
+    </section>
   </>
 }
